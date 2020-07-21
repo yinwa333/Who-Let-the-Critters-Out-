0bf10e9e9a51148304d122e427d20d4f284817a7
Ami Cai <ami.cai333@gmail.com>
2020-07-20 21:51:14 -0400

    Uploading Latest Build
    I added crab movement since the Unsuccessful Repo

---
+++ Who-Let-the-Critters-Out-master/Assets/AC/My Scripts/SimpleEnemyMoveVertical.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemyMoveVertical : MonoBehaviour
+{
+
+    public int EnemySpeed;
+    public int YMoveDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, YMoveDirection));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, YMoveDirection) * EnemySpeed;
+        if (hit.distance < 1.0f)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        if (YMoveDirection < 0)
+        {
+            YMoveDirection = 1;
+        }
+        else
+        {
+            YMoveDirection = -1;
+        }
+    }
+}
